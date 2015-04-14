b677353cecc2d0436fcbd3e7585cf0103ced8239
Chris Trott <chris@twocentstudios.com>
2015-04-13 23:53:40 -0400

    Initial set up for example project

--- Example/HOPStrings/HOPViewController.m
+++ Example/HOPStrings/HOPViewController.m
@@ -10,20 +10,53 @@
 
 @interface HOPViewController ()
 
+@property (nonatomic) UISegmentedControl *segmentedControl;
+@property (nonatomic) UILabel *label;
+
 @end
 
 @implementation HOPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    NSArray *items = @[ @"1", @"2", @"3", @"4", @"5" ];
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:items];
+    [self.segmentedControl addTarget:self action:@selector(segmentDidChange:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.segmentedControl];
+
+    self.label = [[UILabel alloc] init];
+    self.label.text = @"doc troll me";
+    [self.view addSubview:self.label];
+
+    [self.segmentedControl setSelectedSegmentIndex:0];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+
+    CGFloat const viewWidth = CGRectGetWidth(self.view.bounds);
+    self.segmentedControl.frame = CGRectMake(0, 40, viewWidth, 50);
+
+    self.label.frame = CGRectMake(0, 140, viewWidth, 300);
+}
+
+- (void)segmentDidChange:(UISegmentedControl *)control {
+    NSInteger index = [control selectedSegmentIndex];
+    if (index == 0) {
+
+    } else if (index == 1) {
+
+    } else if (index == 2) {
+
+    } else if (index == 3) {
+
+    } else if (index == 4) {
+
+    }
+    NSLog(@"%@", @(index));
 }
 
 @end
