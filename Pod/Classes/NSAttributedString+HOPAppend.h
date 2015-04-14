bbb5e02b24e74f22c185e94e56aa7e1dd47a7cde
Chris Trott <chris@twocentstudios.com>
2015-04-14 12:00:45 -0400

    Docs

--- Pod/Classes/NSAttributedString+HOPAppend.h
+++ Pod/Classes/NSAttributedString+HOPAppend.h
@@ -9,6 +9,7 @@
 
 @interface NSAttributedString (HOPAppend)
 
+/// Create a new NSAttributedString by appending `string` to the receiver.
 - (instancetype)hop_stringByAppendingAttributedString:(NSAttributedString *)string;
 
 @end
