7aa32ed44f76c95d4dc2199e1352d09c444585e7
Chris Trott <chris@twocentstudios.com>
2015-04-14 10:56:16 -0400

    Non-null attributes

--- Pod/Classes/HOPAttributer.h
+++ Pod/Classes/HOPAttributer.h
@@ -17,22 +17,22 @@
 
 # pragma mark - Initialization
 
-- (instancetype)initWithAttributedString:(NSAttributedString *)attributedString defaultAttributes:(HOPStringAttributes *)attributes NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithAttributedString:(NSAttributedString *)attributedString defaultAttributes:(HOPStringAttributes *)attributes __attribute((nonnull)) NS_DESIGNATED_INITIALIZER;
 
 # pragma mark - Lifting
 
-+ (instancetype)attributerWithDefaultAttributes:(HOPStringAttributes *)attributes;
-+ (instancetype)attributerWithDefaultAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock;
-+ (instancetype)attributerWithString:(NSString *)string;
-+ (instancetype)attributerWithString:(NSString *)string defaultAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock;
++ (instancetype)attributerWithDefaultAttributes:(HOPStringAttributes *)attributes __attribute((nonnull));
++ (instancetype)attributerWithDefaultAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock __attribute((nonnull));
++ (instancetype)attributerWithString:(NSString *)string __attribute((nonnull));
++ (instancetype)attributerWithString:(NSString *)string defaultAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock __attribute((nonnull));
 
 # pragma mark - Appending without using default attributes
 
-- (instancetype)appendString:(NSString *)string emptyAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock;
+- (instancetype)appendString:(NSString *)string emptyAttributesBlock:(void(^)(HOPStringAttributes *attr))attributesBlock __attribute((nonnull));
 
-# pragma mark - Appending using overridable default attributes
+# pragma mark - Appending starting with default attributes
 
-- (instancetype)appendString:(NSString *)string;
-- (instancetype)appendString:(NSString *)string attributesBlock:(void (^)(HOPStringAttributes *attr))attributesBlock;
+- (instancetype)appendString:(NSString *)string __attribute((nonnull));
+- (instancetype)appendString:(NSString *)string attributesBlock:(void (^)(HOPStringAttributes *attr))attributesBlock __attribute((nonnull));
 
 @end
