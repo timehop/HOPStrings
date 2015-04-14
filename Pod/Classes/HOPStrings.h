837f0d66738a32e43df770a5a8976cfb52dd3bf8
Chris Trott <chris@twocentstudios.com>
2015-04-13 23:37:26 -0400

    Library header

---
+++ Pod/Classes/HOPStrings.h
@@ -0,0 +1,17 @@
+//
+//  HOPStrings.h
+//  Pods
+//
+//  Created by Chris Trott on 4/13/15.
+//
+//
+
+#ifndef Pods_HOPStrings_h
+#define Pods_HOPStrings_h
+
+#import "HOPString.h"
+#import "HOPStringAttributes.h"
+
+#import "NSAttributedString+HOPAppend.h"
+
+#endif
